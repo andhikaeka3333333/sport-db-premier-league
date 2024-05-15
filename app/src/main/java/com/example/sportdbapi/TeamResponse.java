d88a56f86e64efe966552f35750c6b31faf17667
Andhika Eka <andhikaeka311@gmail.com>
2024-05-15 12:59:10 +0700

    Adding recycler view on Retrofit

---
+++ app/src/main/java/com/example/sportdbapi/TeamResponse.java
@@ -0,0 +1,13 @@
+package com.example.sportdbapi;
+
+import java.util.List;
+
+
+public class TeamResponse {
+    private List<Team> teams;
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+}
+
